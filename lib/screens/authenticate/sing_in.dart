0e902d0e4e3396ac9f21ca2eed7627e8aa84587b
Vishruth <vishruthvs2003@gmail.com>
2023-09-28 23:30:48 +0530

    sing-in with email/password

--- lib/screens/authenticate/sing_in.dart
+++ lib/screens/authenticate/sing_in.dart
@@ -10,6 +10,10 @@ class Singin extends StatefulWidget {
 
 class _SinginState extends State<Singin> {
   final AuthService _auth = AuthService();
+
+  //text field state
+  String email = '';
+  String password = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,20 +25,51 @@ class _SinginState extends State<Singin> {
       ),
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: ElevatedButton(
-          style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all<Color>(Colors.white54),
+        // child: ElevatedButton(
+        //   style: ButtonStyle(
+        //     backgroundColor: MaterialStateProperty.all<Color>(Colors.white54),
+        //   ),
+        //   onPressed: () async {
+        //     dynamic result = await _auth.signinAnonymous();
+        //     if (result == null) {
+        //       print('Error signing in');
+        //     } else {
+        //       print('Signed in succesfully');
+        //       print('User UID: ${result.uid}');
+        //     }
+        //   },
+        //   child: Text('Sing in anom'),
+        // ),
+        child: Form(
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 20.0),
+              TextFormField(onChanged: (val) {
+                setState(() => email = val);
+              }),
+              SizedBox(height: 20.0),
+              TextFormField(
+                  obscureText: true,
+                  onChanged: (val) {
+                    setState(() => password = val);
+                  }),
+              SizedBox(height: 20.0),
+              ElevatedButton(
+                onPressed: () async {
+                  print(email);
+                  print(password);
+                },
+                child: Text(
+                  'Sign in',
+                  style: TextStyle(color: Colors.black87),
+                ),
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.white54),
+                ),
+              )
+            ],
           ),
-          onPressed: () async {
-            dynamic result = await _auth.signinAnonymous();
-            if (result == null) {
-              print('Error signing in');
-            } else {
-              print('Signed in succesfully');
-              print('User UID: ${result.uid}');
-            }
-          },
-          child: Text('Sing in anom'),
         ),
       ),
     );
