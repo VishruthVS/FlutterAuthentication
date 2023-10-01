bb324df862b14db704a9e135f436fd2c42f4109c
Vishruth <vishruthvs2003@gmail.com>
2023-10-02 03:12:33 +0530

    adding sliders acc to user_strengh

--- lib/screens/home/settings_form.dart
+++ lib/screens/home/settings_form.dart
@@ -88,6 +88,16 @@ class _SettingsFormState extends State<SettingsForm> {
             onChanged: (val) => setState(() => _currentSugars = val),
           ),
           //slider
+          Slider(
+              value: (_currentStrength ?? 100).toDouble(),
+              activeColor: Colors.brown[_currentStrength ?? 100]!,
+              inactiveColor: Colors.brown[_currentStrength ?? 100]!,
+              min: 100.0,
+              max: 900.0,
+              divisions: 8,
+              onChanged: (val) => setState(
+                    () => _currentStrength = val.round(),
+                  )),
           ElevatedButton(
             style: ElevatedButton.styleFrom(
               backgroundColor: Colors.pink[400],
